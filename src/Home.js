bd31f85a751cc25a7224e9a81a0ab63dd55b2ada
AWSFilms <vassdanie@gmail.com>
2021-08-24 16:54:59 +0100

    fgdfg

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import germany from '/Users/danielvass/danielwebsite/src/pictures/germany.jpeg';
 
 function About() {
     return (
         <div className="pic1">
-      <img src="https://github.com/eveporcello.png" alt="Girl in a jacket" />
+      <img src={germany} alt="Girl in a jacket" />
     </div>
     );
 }
